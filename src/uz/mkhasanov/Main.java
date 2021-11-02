dd7ea2daaee92f01d926fa332a09d3c6ba0395bc
Mirzokhid <mirzohidkhasanov12@gmail.com>
2021-11-02 09:43:45 +0500

    added problems: Problem_1295_Find_Numbers_with_Even_Number_of_Digits

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -1,27 +1,10 @@
 package uz.mkhasanov;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
-        String s = "aabaaabc";
-        String s1 = "aaaaddddaaadffffggggaaa";
-        String s2 = "ddssssdddffffggssgg";
-//        System.out.println(s.substring(2));
-        System.out.println(doUnique(s));
-        System.out.println(doUnique(s1));
-        System.out.println(doUnique(s2));
-    }
-
-
-    public static String doUnique(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    s = s.substring(0, j) + s.substring(j + 1);
-                    j = s.indexOf(s.charAt(i));
-                }
-            }
-        }
-        return s;
+        System.out.println("aldjsfjlasfhk");
     }
 }
---
+++ src/uz/mkhasanov/P1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,43 @@
+package uz.mkhasanov;
+
+import java.util.Arrays;
+
+public class P1295_Find_Numbers_with_Even_Number_of_Digits {
+    public static void main(String[] args) {
+        int[] a = new int[]{12, 345, 2, 6, 7896};
+        System.out.println(findNumbers2(a));
+    }
+
+
+    ///APPROACH-1
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (String.valueOf(nums[i]).length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    ///APPROACH-2
+    public static int findNumbers2(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int numberOfDigits = 0;
+            while (nums[i] > 0) {
+                nums[i] = nums[i] / 10;
+                numberOfDigits++;
+            }
+
+            if (numberOfDigits % 2 ==0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
