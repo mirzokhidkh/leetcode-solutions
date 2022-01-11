5bcb2514e0477662064de5c3d660e37420b475ca
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-01-11 17:39:24 +0500

    added problems: N-P917_Reverse_Only_Letters

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -1,13 +1,15 @@
 package uz.mkhasanov;
 
 import java.text.ParseException;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException {
 
-        System.out.println((char)((int)'a' + 1));
+
+
+
+
     }
 
 }
