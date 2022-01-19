ce7b61ebe71119a8ede21f1f980e9a26fd77c439
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-01-19 14:24:40 +0500

    added problems: Card -> Queue-BFS

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -1,15 +1,14 @@
 package uz.mkhasanov;
 
+import javafx.util.Pair;
+
 import java.text.ParseException;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException {
 
-
     }
-
-
 
 
 }
