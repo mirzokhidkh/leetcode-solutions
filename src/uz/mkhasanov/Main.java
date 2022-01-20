88c3e79033bec46a7d2dd71e6e55a95f24489c70
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-01-20 16:58:17 +0500

    added problems: Card -> Queue-BFS

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -3,11 +3,15 @@ package uz.mkhasanov;
 import javafx.util.Pair;
 
 import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException {
-
+        Set<Integer> visited = new HashSet<>();
+        System.out.println(visited.add(1));
+        System.out.println(visited.add(1));
     }
 
 
