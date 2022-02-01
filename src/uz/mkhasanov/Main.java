9f8a99d3e6029666439df7d66566822eb08e728a
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-02-01 23:16:18 +0500

    added problems: explore/card -> Stack-DFS

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -2,14 +2,18 @@ package uz.mkhasanov;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException, IOException {
 
-//        System.out.println(Integer.parseInt("-11"));
-
-        System.out.println(6/-132);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
     }
 
 
