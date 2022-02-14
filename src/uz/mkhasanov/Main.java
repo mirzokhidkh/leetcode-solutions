17b96bbbf04dd0052d24e383da10470071b42365
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-02-14 23:59:16 +0500

    added problems: explore/card -> Array and String : Find Pivot Index

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -7,18 +7,18 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) throws ParseException, IOException {
-
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
+        int a = 7, b = 5;
+        swap(a, b);
+        System.out.println(a);
+        System.out.println(b);
     }
 
-
-    
-
-
+    public static void swap(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+//        System.out.println(a);
+//        System.out.println(b);
+    }
 
 }
