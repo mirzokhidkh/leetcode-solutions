da6e2e8b05ab85fd5841f38e27c8d08dd0918ecb
Mirzokhid <mirzohidkhasanov12@gmail.com>
2022-02-16 00:18:24 +0500

    added problems: explore/card -> Array and String : Largest Number At Least Twice of Others

---
+++ src/uz/mkhasanov/arrays/easy/P747_Largest_Number_At_Least_Twice_of_Others.java
@@ -0,0 +1,31 @@
+package uz.mkhasanov.arrays.easy;
+
+import java.util.Arrays;
+
+public class P747_Largest_Number_At_Least_Twice_of_Others {
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 6, 1, 0};
+        System.out.println(dominantIndex(a));
+
+    }
+
+    public static int dominantIndex(int[] nums) {
+        int max = nums[0];
+        int ind = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                ind = i;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=0)
+            if (max / nums[i] < 2 && ind != i)
+                return -1;
+        }
+        return ind;
+    }
+
+
+}
