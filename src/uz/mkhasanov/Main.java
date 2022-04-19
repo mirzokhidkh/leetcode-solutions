7c612afebb1e140dc3ba02ed4146db1fd4ff3fbb
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2022-04-19 20:47:58 +0500

    P1903_Largest_Odd_Number_in_String

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -1,21 +1,44 @@
 package uz.mkhasanov;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        byte a = 1;
-        byte b = 2;
-        byte c = (byte) (a + b);
 
-        List<List<Integer>> list = new ArrayList<>();
+        String s = "52";
 
-        System.out.println(list);
+        String sq = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Integer.parseInt(String.valueOf(s.charAt(i))) % 2 != 0) {
+                for (int j = 0; j < i+1; j++) {
+                    sq = s.substring(j, i+1);
+                    if ((Integer.parseInt(sq.substring(sq.length() - 1)) % 2 != 0)) {
+                        System.out.println(sq);
+                    }
+                }
+            }
+        }
+
 
     }
 
-    
+    public String largestOddNumber(String num) {
+        String s = num;
+        String sq = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Integer.parseInt(String.valueOf(s.charAt(i))) % 2 != 0) {
+                for (int j = 0; j < i; j++) {
+                    sq = num.substring(j, i);
+                    if ((Integer.parseInt(sq.substring(sq.length() - 1)) % 2 != 0)) {
+                        return sq;
+                    }
+                }
+            }
+        }
+        return "";
 
+    }
 }
--- src/uz/mkhasanov/backtracking/medium/P78_Subsets.java
+++ src/uz/mkhasanov/backtracking/medium/P78_Subsets.java
@@ -4,39 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class P78_Subsets {
-    public List<List<Integer>> subsets(int[] nums) {
+
+    public static void main(String[] args) {
+        System.out.println(subsets(new int[]{1, 2, 3}));
+//        // {}
+//        // {1}, {1,2}, {1,2,3}, {1,3}
+//        // {2}, {2,3}
+//        // {3}
+        System.out.println(subsets(new int[]{1, 2, 3, 4}));
+
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
 
         List<List<Integer>> list = new ArrayList<>();
         list.add(new ArrayList<>());
 
         List<Integer> subset = new ArrayList<>();
 
-        int i = 0;
-        while(i < nums.length){
-            // subset.add(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
 
-            int k = 0;
+            subset.add(nums[i]);
+            list.add(subset);
 
-            while(k < nums.length){
-                subset = new ArrayList<>();
-                int j = i;
-                int l = j + k + 1;
+            subset = new ArrayList<>();
 
-                while(j < l && j < nums.length){
-                    subset.add(nums[j]);
-                    j++;
+            for (int l = i; l < nums.length; l++) {
+
+                for (int j = l + 1; j < nums.length; j++) {
+                    subset.add(nums[i]);
+
+                    for (int k = l + 1; k < j + 1; k++) {
+                        subset.add(nums[k]);
+                    }
+                    list.add(subset);
+                    subset = new ArrayList<>();
+
                 }
-                k++;
-                list.add(subset);
-            }
 
-            i++;
+
+            }
 
         }
 
         return list;
     }
 
-    
 
 }
