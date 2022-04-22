15d9d3012165416940da31b7bc0fc6914e2c1c9d
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2022-04-22 20:35:35 +0500

    P78_Subsets

--- src/uz/mkhasanov/Main.java
+++ src/uz/mkhasanov/Main.java
@@ -4,41 +4,21 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        String s = "52";
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(5);
+        myStack.push(6);
+        myStack.push(7);
 
-        String sq = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (Integer.parseInt(String.valueOf(s.charAt(i))) % 2 != 0) {
-                for (int j = 0; j < i+1; j++) {
-                    sq = s.substring(j, i+1);
-                    if ((Integer.parseInt(sq.substring(sq.length() - 1)) % 2 != 0)) {
-                        System.out.println(sq);
-                    }
-                }
-            }
-        }
+        System.out.println(myStack);
+        myStack.pop();
+        System.out.println(myStack);
 
 
     }
 
-    public String largestOddNumber(String num) {
-        String s = num;
-        String sq = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (Integer.parseInt(String.valueOf(s.charAt(i))) % 2 != 0) {
-                for (int j = 0; j < i; j++) {
-                    sq = num.substring(j, i);
-                    if ((Integer.parseInt(sq.substring(sq.length() - 1)) % 2 != 0)) {
-                        return sq;
-                    }
-                }
-            }
-        }
-        return "";
-
-    }
 }
--- src/uz/mkhasanov/backtracking/medium/P78_Subsets.java
+++ src/uz/mkhasanov/backtracking/medium/P78_Subsets.java
@@ -1,7 +1,6 @@
 package uz.mkhasanov.backtracking.medium;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class P78_Subsets {
 
@@ -11,44 +10,33 @@ public class P78_Subsets {
 //        // {1}, {1,2}, {1,2,3}, {1,3}
 //        // {2}, {2,3}
 //        // {3}
-        System.out.println(subsets(new int[]{1, 2, 3, 4}));
+//        System.out.println(subsets(new int[]{1, 2, 3, 4}));
 
     }
+
+
+    private static final List<List<Integer>> res = new ArrayList<>();
+    private static Stack<Integer> subset = new Stack<>();
 
     public static List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(new ArrayList<>());
-
-        List<Integer> subset = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-
-            subset.add(nums[i]);
-            list.add(subset);
-
-            subset = new ArrayList<>();
-
-            for (int l = i; l < nums.length; l++) {
-
-                for (int j = l + 1; j < nums.length; j++) {
-                    subset.add(nums[i]);
-
-                    for (int k = l + 1; k < j + 1; k++) {
-                        subset.add(nums[k]);
-                    }
-                    list.add(subset);
-                    subset = new ArrayList<>();
-
-                }
-
-
-            }
-
-        }
-
-        return list;
+        dfs(0,nums);
+        return res;
     }
 
+
+    private static void dfs(int i, int[] nums){
+
+        if (i>=nums.length){
+            System.out.println(subset);
+            res.add(new ArrayList(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        dfs(i+1,nums);
+
+        subset.pop();
+        dfs(i+1,nums);
+    }
 
 }
