4b84870b3e81bc2c2be603f0df459c1274305562
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2022-05-04 01:06:17 +0500

    1295 Find_Numbers_with_Even_Number_of_Digits

--- src/uz/mirzokhidkh/arrays/easy/P1295_Find_Numbers_with_Even_Number_of_Digits.java
+++ src/uz/mirzokhidkh/arrays/easy/P1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -3,13 +3,15 @@ package uz.mirzokhidkh.arrays.easy;
 public class P1295_Find_Numbers_with_Even_Number_of_Digits {
     public static void main(String[] args) {
         int[] a = new int[]{12, 345, 2, 6, 7896};
-        System.out.println(findNumbers2(a));
+        System.out.println(findNumbers1(a));
     }
 
 
-    ///APPROACH-1
+    /**
+     * APPROACH-1
+     **/
 
-    public static int findNumbers(int[] nums) {
+    public static int findNumbers1(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (String.valueOf(nums[i]).length() % 2 == 0) {
@@ -19,23 +21,27 @@ public class P1295_Find_Numbers_with_Even_Number_of_Digits {
         return count;
     }
 
-    ///APPROACH-2
-    public static int findNumbers2(int[] nums) {
-        int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int numberOfDigits = 0;
-            while (nums[i] > 0) {
-                nums[i] = nums[i] / 10;
-                numberOfDigits++;
-            }
-
-            if (numberOfDigits % 2 ==0) {
-                count++;
+    /**
+     * APPROACH-2
+     **/
+    public int findNumbers2(int[] nums) {
+        int k = 0;
+        for (int num : nums) {
+            if (isEvenNumberOfDigits(num)) {
+                k++;
             }
         }
+        return k;
+    }
 
-        return count;
+    public boolean isEvenNumberOfDigits(int num) {
+        int k = 0;
+        while (num != 0) {
+            num /= 10;
+            k++;
+        }
+        return k % 2 == 0;
     }
 
 }
