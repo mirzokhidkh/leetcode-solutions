db1679f3e4d4502f383a7860ca90ff3e26287fc6
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2022-05-05 17:04:50 +0500

    1089. Duplicate Zeros

--- src/uz/mirzokhidkh/arrays/easy/P1089_Duplicate_Zeros.java
+++ src/uz/mirzokhidkh/arrays/easy/P1089_Duplicate_Zeros.java
@@ -3,9 +3,9 @@ package uz.mirzokhidkh.arrays.easy;
 public class P1089_Duplicate_Zeros {
 
 
-    /*
+    /**
     APPROACH-1
-     */
+     **/
     public void duplicateZeros(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
