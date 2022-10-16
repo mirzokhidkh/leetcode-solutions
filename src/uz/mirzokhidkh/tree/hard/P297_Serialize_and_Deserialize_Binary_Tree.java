0cc35b327b6695e8bdb840584e6190a55d89b1bf
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2022-10-17 03:20:06 +0500

    problem 297 has been added

--- src/uz/mirzokhidkh/tree/hard/P297_Serialize_and_Deserialize_Binary_Tree.java
+++ src/uz/mirzokhidkh/tree/hard/P297_Serialize_and_Deserialize_Binary_Tree.java
@@ -11,12 +11,31 @@ public class P297_Serialize_and_Deserialize_Binary_Tree {
         root.right = new TreeNode(3);
         root.right.left = new TreeNode(4);
         root.right.right = new TreeNode(5);
+        String s1 = serialize(root);
+        System.out.println(s1);
+        TreeNode treeNode1 = deserialize(s1);
+        System.out.println(serialize(treeNode1));
+
 
         TreeNode root1 = new TreeNode(1);
         root1.left = new TreeNode(2);
+//        System.out.println(serialize(root1));
+        String s2 = serialize(root1);
+        System.out.println(s2);
+        TreeNode treeNode2 = deserialize(s2);
+        System.out.println(serialize(treeNode2));
 
-        System.out.println(serialize(root1));
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+        root2.left.right = new TreeNode(5);
+        root2.right.left = new TreeNode(4);
+        String s3 = serialize(root2);
+        System.out.println(s3);
+        TreeNode treeNode3 = deserialize(s3);
+        System.out.println(serialize(treeNode3));
 
+//        System.out.println(serialize(root2));
 
 //        String s = "[1,2,3,null,null,4,5]";
         String s = "[1,2]";
@@ -31,8 +50,54 @@ public class P297_Serialize_and_Deserialize_Binary_Tree {
 //        System.out.println(serialize(treeNode));
     }
 
+    public static String serialize(TreeNode root) {
+        if (root == null) return "[]";
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<List<String>> listOfStrLists = new ArrayList<>();
+        listOfStrLists.add(Collections.singletonList(String.valueOf(root.val)));
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            List<String> levelList = new ArrayList<>();
+
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                addTo(queue, levelList, node.left);
+
+                addTo(queue, levelList, node.right);
+            }
+
+            if (!queue.isEmpty()) {
+                listOfStrLists.add(levelList);
+            }
+
+        }
+
+        List<String> strList = new ArrayList<>();
+        listOfStrLists.forEach(strList::addAll);
+
+        String str = String.join(",", strList);
+
+        return "[" + (str.endsWith("null") ? str.substring(0, str.length() - 5) : str) + "]";
+
+    }
+
+    private static void addTo(Queue<TreeNode> queue, List<String> levelList, TreeNode left) {
+        if (left != null) {
+            queue.offer(left);
+            levelList.add(String.valueOf(left.val));
+        } else {
+            levelList.add("null");
+        }
+    }
+
     public static TreeNode deserialize(String data) {
         if (data == "[]") return null;
+
         data = data.substring(1, data.length() - 1);
         String[] arr = data.split(",");
         Queue<String> queue = new LinkedList<>(Arrays.asList(arr));
@@ -41,13 +106,6 @@ public class P297_Serialize_and_Deserialize_Binary_Tree {
 
         Queue<TreeNode> queueNode = new LinkedList<>();
         queueNode.offer(root);
-        // if (node.left != null) {
-        // queue.offer(node.left);
-//         }
-
-//         if (node.right != null) {
-//             queue.offer(node.right);
-//         }
 
         while (!queue.isEmpty()) {
             int size = queueNode.size();
@@ -55,66 +113,29 @@ public class P297_Serialize_and_Deserialize_Binary_Tree {
             while (size-- > 0) {
                 TreeNode node = queueNode.poll();
 
-                if (!queue.isEmpty()) {
-                    String val = queue.poll();
-                    if (!Objects.equals(val, "null")) {
-                        node.left = new TreeNode(Integer.parseInt(val));
-                        queueNode.offer(node.left);
-                    }
-                }
-
-                if (!queue.isEmpty()) {
-                    String val = queue.poll();
-
-                    if (!Objects.equals(val, "null")) {
-                        node.right = new TreeNode(Integer.parseInt(val));
-                        queueNode.offer(node.right);
-                    }
-                }
-
-
+                addAsChild(queue, queueNode, node, true);
+                addAsChild(queue, queueNode, node, false);
             }
         }
-
 
         return root;
     }
 
-    public static String serialize(TreeNode root) {
-        if (root == null) return "[]";
-        String str = "";
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        List<String> list = new ArrayList<>();
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            List<String> z = new ArrayList<>();
-            while (size-- > 0) {
-                TreeNode node = queue.poll();
-
-                list.add(String.valueOf(node.val));
-
-                if (node.left != null) {
-                    queue.offer(node.left);
-                } else {
-                    z.add("null");
+    private static void addAsChild(Queue<String> queue, Queue<TreeNode> queueNode, TreeNode node, boolean isLeft) {
+        if (!queue.isEmpty()) {
+            String val = queue.poll();
+            if (!Objects.equals(val, "null")) {
+                if (isLeft){
+                    node.left = new TreeNode(Integer.parseInt(val));
+                    queueNode.offer(node.left);
+                }else {
+                    node.right = new TreeNode(Integer.parseInt(val));
+                    queueNode.offer(node.right);
                 }
 
-                if (node.right != null) {
-                    queue.offer(node.right);
-                } else {
-                    z.add("null");
-                }
             }
-            if (!queue.isEmpty())
-                list.addAll(z);
         }
-
-        str = String.join(",", list);
-
-        return "[" + str + "]";
     }
+
+
 }
