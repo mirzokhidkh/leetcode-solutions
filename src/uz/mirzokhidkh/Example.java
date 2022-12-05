1959e22b61529dad7d4af4bed1ac57159533f1d1
mirzokhidkh <mirzohidkhasanov12@gmail.com>
2022-12-05 11:11:56 +0500

    added recursive problems, database solutions and adventofcode.com problem solvings

--- src/uz/mirzokhidkh/Example.java
+++ src/uz/mirzokhidkh/Example.java
@@ -1,37 +1,15 @@
 package uz.mirzokhidkh;
 
-import java.util.*;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 
 public class Example {
 
-    public static void main(String[] args) {
-//
-//        System.out.println('a'>=97);
-//
-//
-//        Stack<Integer> stack= new Stack<>();
-//        for (int i = 0; i <10 ; i++) {
-//
-//            if (i==1){
-//                stack.push(i);
-//            }
-//        }
-//
-//
-//        System.out.println(stack);
+    public static void main(String[] args) throws MalformedURLException, UnsupportedEncodingException {
 
-
-        Map<Integer,Integer> map = new HashMap<>();
-
-        map.put(1,1);
-        map.put(2,2);
-        map.put(3,3);
-
-        System.out.println(map);
 
 
     }
-
 
 
 }
