b3ab62fb69b4497e958bda93714f418b2db157d2
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2022-12-09 18:25:35 +0500

    added recursive problems, database solutions and adventofcode.com problem solvings

---
+++ src/uz/mirzokhidkh/adventofcode/day05/Main.java
@@ -0,0 +1,28 @@
+package uz.mirzokhidkh.adventofcode.day05;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/day05/input.txt"));
+
+        int total1 = 0;
+
+        int total2 = 0;
+
+        while (sc.hasNext()) {
+            String strLine = sc.nextLine();
+
+
+
+        }
+
+        System.out.println(total1);
+        System.out.println(total2);
+    }
+
+
+}
