39c2ea74a24b26cdfce0a7ce0b0711b031e3a158
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2022-12-20 11:30:58 +0500

    for test

--- src/uz/mirzokhidkh/Solution.java
+++ src/uz/mirzokhidkh/Solution.java
@@ -4,36 +4,15 @@ public class Solution {
 
     public static void main(String[] args) {
 
-//        Map<Integer, Integer> map = new HashMap<>();
-//
-//        Set<Integer> set = new HashSet<>();
-//
-//
-////        List<String> list = new ArrayList<String>() {{
-////            add("S");
-////        }};
-//
-//
-////        boolean a=false;
-////        System.out.println(a);
-//
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        list.add(4);
-//
-//        List<Integer> list2 = new ArrayList<>();
-//        list.add(5);
-//        list.add(7);
-//        list.addAll(list2);
-//
-//
-//        String join = list.stream().map(String::valueOf).collect(Collectors.joining(","));
-//        System.out.println(join);
+//        System.out.println('b'-'a'+1);
+//        String s = "abcd";
+//        System.out.println(s.substring(0,s.length()/2));
+//        System.out.println(s.substring(s.length()/2));
 
-
-
+        System.out.println('p'-'a'+1);
+        System.out.println('A'-'A'+27);
+        System.out.println('B'-'A'+27);
+        System.out.println('C'-'A'+27);
 
     }
 }
