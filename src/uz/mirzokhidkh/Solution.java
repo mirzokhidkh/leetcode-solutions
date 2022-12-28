ac20398559dd7aeb398eca5384436459d9486efb
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2022-12-28 19:49:45 +0500

    for test

--- src/uz/mirzokhidkh/Solution.java
+++ src/uz/mirzokhidkh/Solution.java
@@ -4,18 +4,9 @@ public class Solution {
 
     public static void main(String[] args) {
 
-//        System.out.println('b'-'a'+1);
-//        String s = "abcd";
-//        System.out.println(s.substring(0,s.length()/2));
-//        System.out.println(s.substring(s.length()/2));
-
-        System.out.println('p'-'a'+1);
-        System.out.println('A'-'A'+27);
-        System.out.println('B'-'A'+27);
-        System.out.println('C'-'A'+27);
-
-
 
 
     }
+
+
 }
