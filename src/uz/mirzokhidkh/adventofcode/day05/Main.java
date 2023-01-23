f79c8337c8824bd68e980ab299a4f6b6268db7ca
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-01-23 20:33:26 +0500

    for test

--- src/uz/mirzokhidkh/adventofcode/day05/Main.java
+++ src/uz/mirzokhidkh/adventofcode/day05/Main.java
@@ -1,28 +1,43 @@
 package uz.mirzokhidkh.adventofcode.day05;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/day05/input.txt"));
+//        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/day05/input.txt"));
+//
+//        int total1 = 0;
+//
+//        int total2 = 0;
+//
+//        while (sc.hasNext()) {
+//            String strLine = sc.nextLine();
+//
+//
+//
+//        }
+//
+//        System.out.println(total1);
+//        System.out.println(total2);
 
-        int total1 = 0;
 
-        int total2 = 0;
+
+        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/day05/ms.txt"));
+
+        FileWriter fileWriter = new FileWriter("output.txt");
+        int i=0;
 
         while (sc.hasNext()) {
             String strLine = sc.nextLine();
 
+            fileWriter.write(++i+" ===> "+strLine+"\n");
+            fileWriter.flush();
 
 
         }
-
-        System.out.println(total1);
-        System.out.println(total2);
     }
-
-
 }
