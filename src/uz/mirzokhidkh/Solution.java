4116fe1bfdeb7d3190aa676ababc28d43af2ddbb
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-04-03 22:00:12 +0500

    added c++ solutions

--- src/uz/mirzokhidkh/Solution.java
+++ src/uz/mirzokhidkh/Solution.java
@@ -2,14 +2,22 @@ package uz.mirzokhidkh;
 
 public class Solution {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        long start = System.currentTimeMillis();
-        Thread.sleep(3000);
-        long end = System.currentTimeMillis();
-        System.out.println((end-start)/1000);
-
+    //    Задано рекуррентное соотношение
+//    f(n) =  f(n+3) - f(n+2)
+//    f(1) = 1
+//    f(2) = 2
+//    f(3) = 3
+//    Написать программу вычисляющую сумму первых 100 элементов последовательности.
+//    Ответом должен быть архив с кодом и значение вычисленной суммы поделенное по модулю на 10000.
+    public static void main(String[] args){
+        System.out.println(func(4));
     }
 
+
+    public static int func(int n) {
+        if (n < 4) return n;
+
+        return func(n - 3) - func(n - 2);
+    }
 
 }
