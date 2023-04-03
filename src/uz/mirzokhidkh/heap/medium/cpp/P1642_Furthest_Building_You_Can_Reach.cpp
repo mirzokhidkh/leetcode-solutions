b83a8464611bdd2529922ed9884baffb2bb4f8a3
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-04-03 22:19:25 +0500

    added c++ solutions

--- src/uz/mirzokhidkh/heap/medium/cpp/P1642_Furthest_Building_You_Can_Reach.cpp
+++ src/uz/mirzokhidkh/heap/medium/cpp/P1642_Furthest_Building_You_Can_Reach.cpp
@@ -1,27 +1,51 @@
+#include <algorithm>
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> maxHeap;
 
-        int i;
-        for(i=0; i<heights.size()-1; i++){
-            if(heights[i+1] <= heights[i]){
-                continue;
-            }
+        //PLAN
+        //find all variations and push index to maxHeap step by step until there are no ladders and bricks
+        //finally return the top of th maxHeap which the furthest building index
 
-            if(bricks == 0 && ladders == 0 ){
-                return i;
-            }
-
-
-            int dif = heights[i+1]-heights[i];
-
-            if(bricks >= dif){
-                bricks -= dif;
-            }else if(ladders != 0){
-                --ladders;
-            }
-        }
-
-        return i;
+        helper(heights,0,bricks,ladders,maxHeap)
+        return maxHeap.top();
     }
+
+    int helper(vector<int>& heights,int i, int bricks, int ladders){
+
+
+    }
+
+
 };
+
+
+//class Solution {
+//public:
+//    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+//
+//        int i;
+//        for(i=0; i<heights.size()-1; i++){
+//            if(heights[i+1] <= heights[i]){
+//                continue;
+//            }
+//
+//            if(bricks == 0 && ladders == 0 ){
+//                return i;
+//            }
+//
+//
+//            int dif = heights[i+1]-heights[i];
+//
+//            if(bricks >= dif){
+//                bricks -= dif;
+//            }else if(ladders != 0){
+//                --ladders;
+//            }
+//        }
+//
+//        return i;
+//    }
+//};
+//
