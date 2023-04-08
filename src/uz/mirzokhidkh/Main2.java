4b069305b5020ef7b29ece2cd7ecd6d2491cb4c2
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-04-08 14:44:04 +0500

    test

---
+++ src/uz/mirzokhidkh/Main2.java
@@ -0,0 +1,13 @@
+package uz.mirzokhidkh;
+
+abstract class Main2 extends javafx.application.Application {
+    static {
+        System.out.println("ss");
+        System.exit(0);
+    }
+
+
+
+
+
+}
