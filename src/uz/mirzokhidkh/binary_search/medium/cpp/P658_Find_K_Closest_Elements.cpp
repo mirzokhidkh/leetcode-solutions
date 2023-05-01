8c290e9c70a36b1842f3a70539e7c30f7399e82f
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-05-01 22:16:20 +0500

    added problems related to Binary Search in C++

--- src/uz/mirzokhidkh/binary_search/medium/cpp/P658_Find_K_Closest_Elements.cpp
+++ src/uz/mirzokhidkh/binary_search/medium/cpp/P658_Find_K_Closest_Elements.cpp
@@ -1,43 +1,56 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    vector<int> findClosestElements(vector<int>& nums, int k, int x) {
+        int n = nums.size()-1;
         vector<int> res;
-        int ind = binarySearch(arr,x,0,arr.size()-1);
-        if(ind != -1){
-            int l = ind - k/2;
-            while(l < ind+1){
-                res.push_back(arr[l]);
-                l++;
-            }
-            int r = ind+1;
-            int s = r + (k%2 == 0 ? k/2-1 : k/2);
-            while(r < s){
-                res.push_back(arr[r]);
+        int ind = binarySearch(nums,x);
+        int l = ind-1, r= ind+1;
+        res.push_back(nums[ind]);
+        while(k > 1 && l >= 0 && r <= n){
+            if(abs(nums[l]-x) <= abs(nums[r]-x)){
+                res.insert(res.begin()+0, nums[l]);
+                l--;
+            }else{
+                res.push_back(nums[r]);
                 r++;
             }
-        }else{
-            int i = 0;
-            while(i<k){
-                res.push_back(arr[i]);
-                i++;
-            }
+            k--;
         }
 
+        while(k > 1){
+            if(l < 0){
+                res.push_back(nums[r]);
+                r++;
+            }else{
+                res.insert(res.begin()+0, nums[l]);
+                l--;
+            }
+            k--;
+        }
         return res;
     }
 
-    int binarySearch(vector<int>& nums, int x, int l, int r){
-        if(l <= r){
+
+  int binarySearch(vector<int>& nums, int x){
+        int l = 0, r = nums.size()-1;
+
+        while(l+1 < r){
             int m = l + (r - l)/2;
 
-             if(nums[m] == x){
-                 return m;
-             }else if(nums[m] < x){
-                 return binarySearch(nums,x,m+1,r);
-             }else{
-                 return binarySearch(nums,x,l,m-1);
-             }
+            if(nums[m] == x){
+                return m;
+            }else if(nums[m] < x){
+                l = m;
+            }else{
+                r = m;
+            }
         }
-        return -1;
+
+        // cout << "nums[l] = "<<nums[l]<<endl;
+        // cout << "nums[r] = "<<nums[r]<<endl;
+        if(abs(nums[l]-x) <= abs(nums[r]-x)){
+            return l;
+        }
+        return r;
     }
 };
