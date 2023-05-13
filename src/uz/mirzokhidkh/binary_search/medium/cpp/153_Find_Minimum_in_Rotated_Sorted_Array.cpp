41948a2f1b50b55998329a03af2c1c946adf2c05
mirzokhidkh <mirzohidkhasanov12@gmail.com>
2023-05-13 20:29:39 +0500

    added solutions related t Binary Search in c++

--- src/uz/mirzokhidkh/binary_search/medium/cpp/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ src/uz/mirzokhidkh/binary_search/medium/cpp/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -16,4 +16,31 @@ public:
     }
 
 
+    // APPROACH-2
+     int findMin2(vector<int>& nums) {
+
+            if(nums.size() == 1){
+                return nums[0];
+            }
+
+            int l = 0, r = nums.size()-1;
+
+            if(nums[r] > nums[0]){
+                return nums[0];
+            }
+
+            while(l < r){
+                int m = l + (r - l)/2;
+
+                if(nums[m] >= nums[0]){
+                    l = m + 1;
+                }else{
+                    r = m;
+                }
+            }
+
+            return nums[l];
+        }
+
+
 };
