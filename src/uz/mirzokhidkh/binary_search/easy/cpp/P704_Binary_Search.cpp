a02a0c960cc9289c146e8c873703e707de7790fe
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-05-19 18:01:33 +0500

    added problems related to Binary Search in C++

--- src/uz/mirzokhidkh/binary_search/easy/cpp/P704_Binary_Search.cpp
+++ src/uz/mirzokhidkh/binary_search/easy/cpp/P704_Binary_Search.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        return binarySearch(nums, target, 0, nums.size()-1);
-    }
 
     int binarySearch(vector<int>& nums, int x, int l, int r){
-
         if(l <= r){
-            int m = l + (r-l)/2;
+            int m = l + (r - l)/2;
 
-            if(nums[m] == x){
-                return m;
-            }else if(x > nums[m]){
-                return binarySearch(nums,x,m+1,r);
-            }else{
-                return binarySearch(nums,x,l,m-1);
-            }
-
+             if(nums[m] == x){
+                 return m;
+             }else if(nums[m] < x){
+                 return binarySearch(nums,x,m+1,r);
+             }else{
+                 return binarySearch(nums,x,l,m-1);
+             }
         }
         return -1;
     }
+
+      int binarySearch(vector<int>& arr, int x){
+            if (arr.size() == 0){
+                return -1;
+            }
+            int l = 0, r = arr.size()-1;
+            while(l <= r){
+                int m = l + (r - l)/2;
+                if(arr[m] == x){
+                    return m;
+                }else if(arr[m] < x){
+                    l = m+1;
+                }else{
+                    r = m-1;
+                }
+            }
+            return -1;
+        }
+
 };
