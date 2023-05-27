2fa6973c288f8490772de15e4111a95d92a61882
mirzokhidkh <mirzohidkhasanov12@gmail.com>
2023-05-27 23:15:08 +0500

    added solutions related t Binary Search in c++

--- src/uz/mirzokhidkh/binary_search/medium/cpp/P/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
+++ src/uz/mirzokhidkh/binary_search/medium/cpp/P/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -2,42 +2,34 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int x) {
 
-        int ind = binarySearch(arr,x);
-        // if(ind == 0){
-        //     return {ind+1,binarySearch(arr,x-arr[ind])+1};
-        // }
-        cout <<"ind = "<< ind;
+        // cout <<"ind = "<< ind;
         int l = 0 ;
-        // int r = ind;
-        int r = ind < arr.size() ? ind : arr.size()-1;
+        int r = binarySearch(arr,x);
 
-        if(arr[r] == x && arr[r] < 0){
-            return {r+1,r+2};
-        }
 
         while(l < r){
             int sum =  arr[l] + arr[r];
             if(sum == x){
                 return {l+1,r+1};
-                // break;
             }else if(sum < x){
                 l++;
             }else{
                 r--;
             }
         }
-        return {-1,-1};
+        return {};
     }
 
 
     int binarySearch(vector<int>& arr, int x){
         int l = 0, r = arr.size()-1;
-
+        int  i = 0;
         while(l <= r){
             int m = l + (r - l)/2;
 
-            if(m+1 < arr.size() && arr[m] <= x  && arr[m+1] > x){
-                return m;
+            if(m+1 < arr.size() && arr[m] < x  && arr[m+1] > x){
+                i = m;
+                break;
             }else if(arr[m] < x){
                 l = m + 1;
             }else{
@@ -45,8 +37,12 @@ public:
             }
         }
 
-        return l;
+        return (i > 0 && i < arr.size() && arr[i] >= 0) ? i : arr.size()-1;
     }
+
+
+
+
 
 
 };
