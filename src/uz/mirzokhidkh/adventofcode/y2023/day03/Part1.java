f4b91418796e3c5fa45eb55e409875a7b1693108
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2023-12-07 17:47:17 +0500

    changes

--- src/uz/mirzokhidkh/adventofcode/y2023/day03/Part1.java
+++ src/uz/mirzokhidkh/adventofcode/y2023/day03/Part1.java
@@ -10,8 +10,8 @@ public class Part1 {
     public static void main(String[] args) throws IOException {
 
         //https://adventofcode.com/2023/day/1/input
-//        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/y2023/day03/input"));
-        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/y2023/day03/test"));
+        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/y2023/day03/input"));
+//        Scanner sc = new Scanner(new File("src/uz/mirzokhidkh/adventofcode/y2023/day03/test"));
         int i = 1;
 
         // 1-> hamma line larni tartib bilan Map ga qo'shib chiqamiz
@@ -25,63 +25,76 @@ public class Part1 {
             String line = sc.nextLine();
             lineMap.put(i++, line);
         }
+//        i = 0;
 
         int sum = 0;
 
-        for (Integer j : lineMap.keySet()) {
-            String line = lineMap.get(j);
+        for (Integer lineNumber : lineMap.keySet()) {
+            String line = lineMap.get(lineNumber);
             StringBuilder num = new StringBuilder();
-            int start = -1, end = -1;
-            System.out.println(j + "-> " + line);
-            for (int k = 0; k < line.length(); k++) {
-                char c = line.charAt(k);
+            int start = -1, end;
+            System.out.println(lineNumber + "-> " + line);
+            for (i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
                 if (Character.isDigit(c)) {
                     num.append(c);
                     if (start == -1) {
-                        start = k;
+                        start = i;
                     }
                 } else if (num.toString().length() > 0) {
-                    end = k - 1;
-
-                    if (start > 0 && isSymbol(String.valueOf(line.charAt(start - 1)))) {
-                        sum += Integer.parseInt(num.toString());
-                        start = -1;
-                        System.out.println(j + "-" + num);
-
-                    } else if (end < line.length() - 1 && isSymbol(String.valueOf(line.charAt(end + 1)))) {
-                        sum += Integer.parseInt(num.toString());
-                        start = -1;
-                        System.out.println(j + "-" + num);
-
-                    } else {
-                        if (j > 1) {
-                            String prevLine = lineMap.get(j - 1);
-                            prevLine = prevLine.substring(start > 0 ? start - 1 : start, end + 2 < line.length() ? end + 2 : end + 1);
-                            for (int pi = 0; pi < prevLine.length(); pi++) {
-                                if (isSymbol(String.valueOf(prevLine.charAt(pi)))) {
-                                    sum += Integer.parseInt(num.toString());
-                                    start = -1;
-                                    System.out.println(j + "-" + num);
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (start != -1 && lineMap.containsKey(j + 1)) {
-                            String nextLine = lineMap.get(j + 1);
-                            nextLine = nextLine.substring(start > 0 ? start - 1 : start, end + 2 < line.length() ? end + 2 : end + 1);
-                            for (int pi = 0; pi < nextLine.length(); pi++) {
-                                if (isSymbol(String.valueOf(nextLine.charAt(pi)))) {
-                                    sum += Integer.parseInt(num.toString());
-                                    start = -1;
-                                    System.out.println(j + "-" + num);
-                                    break;
-                                }
-                            }
+                    end = i - 1;
+                    if (start > 0) {
+                        char cleft = line.charAt(start - 1);
+                        if (isSymbol(cleft)) {
+                            sum += Integer.parseInt(num.toString());
+//                        start = -1;
+                            System.out.println(lineNumber + "-" + num + "_left_" + cleft);
+                            break;
                         }
 
                     }
 
+                    if (end < line.length() - 1) {
+                        char cRight = line.charAt(end + 1);
+                        if (isSymbol(cRight)) {
+
+                            sum += Integer.parseInt(num.toString());
+//                        start = -1;
+                            System.out.println(lineNumber + "-" + num + "_right_" + cRight);
+                            break;
+                        }
+                    }
+
+                    if (lineNumber > 1) {
+                        String prevLine = lineMap.get(lineNumber - 1);
+                        prevLine = prevLine.substring(start > 0 ? start - 1 : start, end + 2 < line.length() ? end + 2 : end + 1);
+                        for (int pi = 0; pi < prevLine.length(); pi++) {
+                            char prevChar = prevLine.charAt(pi);
+                            if (isSymbol(prevChar)) {
+                                sum += Integer.parseInt(num.toString());
+//                                    start = -1;
+                                System.out.println(lineNumber + "-" + num + "_up_" + prevChar);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (start != -1 && lineMap.containsKey(lineNumber + 1)) {
+                        String nextLine = lineMap.get(lineNumber + 1);
+                        nextLine = nextLine.substring(start > 0 ? start - 1 : start, end + 2 < line.length() ? end + 2 : end + 1);
+                        for (int pi = 0; pi < nextLine.length(); pi++) {
+                            char nextChar = nextLine.charAt(pi);
+                            if (isSymbol(nextChar)) {
+                                sum += Integer.parseInt(num.toString());
+//                                    start = -1;
+                                System.out.println(lineNumber + "-" + num + "_down_" + nextChar);
+                                break;
+                            }
+                        }
+                    }
+
+
+                    start = -1;
                     num = new StringBuilder();
 
                 }
@@ -91,8 +104,10 @@ public class Part1 {
         System.out.println("SUM => " + sum);
     }
 
-    public static boolean isSymbol(String s) {
-        return !s.equals(".") && s.matches("[^A-Za-z0-9 ]");
+    public static boolean isSymbol(Character symbol) {
+        String s = String.valueOf(symbol);
+//        return !s.equals(".") && s.matches("[^0-9]");
+        return !s.equals(".");
     }
 
 
