7d65c3cff62e8a8c5b0c5ddc2000e35e55ba602d
mirzokhidkh <mirzohidkhasanov12@gmail.com>
2023-12-17 01:14:28 +0500

    added adventOfCode problems (day-16)

--- src/uz/mirzokhidkh/adventofcode/y2023/day16/Part2.java
+++ src/uz/mirzokhidkh/adventofcode/y2023/day16/Part2.java
@@ -1,11 +1,90 @@
 package uz.mirzokhidkh.adventofcode.y2023.day16;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static uz.mirzokhidkh.adventofcode.y2023.day16.Util.*;
 
 public class Part2 {
     public static void main(String[] args) throws IOException {
 
 
+        char[][] grid = Files.readAllLines(Paths.get("src/uz/mirzokhidkh/adventofcode/y2023/day16/input"))
+//        char[][] grid = Files.readAllLines(Paths.get("src/uz/mirzokhidkh/adventofcode/y2023/day16/test"))
+                .stream()
+                .map(String::toCharArray)
+                .toArray(char[][]::new);
+
+
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                Queue<Tile> queue = new LinkedList<>();
+                if (i == 0) {
+                    boolean[][] visited = new boolean[grid.length][grid[0].length];
+                    queue.offer(new Tile(i, j, UP));
+                    int count = bfs(grid, queue, visited);
+                    int k = getK(grid, visited);
+                    max = Math.max(max, k);
+                }
+
+                if (i == grid.length-1) {
+                    boolean[][] visited = new boolean[grid.length][grid[0].length];
+                    queue.offer(new Tile(i, j, DOWN));
+                    int count = bfs(grid, queue, visited);
+                    int k = getK(grid, visited);
+                    max = Math.max(max, k);
+                }
+
+                if (j == 0) {
+                    boolean[][] visited = new boolean[grid.length][grid[0].length];
+                    queue.offer(new Tile(i, j, LEFT));
+                    int count = bfs(grid, queue, visited);
+                    int k = getK(grid, visited);
+                    max = Math.max(max, k);
+                }
+
+                if (j == grid.length-1) {
+                    boolean[][] visited = new boolean[grid.length][grid[0].length];
+                    queue.offer(new Tile(i, j, RIGHT));
+                    int count = bfs(grid, queue, visited);
+                    int k = getK(grid, visited);
+                    max = Math.max(max, k);
+                }
+
+
+            }
+        }
+
+
+        System.out.println(max);
+
+
+//        System.out.println(Arrays.deepToString(grid));
+
+
+    }
+
+    private static int getK(char[][] grid, boolean[][] visited) {
+        int k = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                boolean isVisited = visited[i][j];
+                if (isVisited) {
+//                                System.out.print("#");
+                    k++;
+                } else {
+//                                System.out.print(".");
+                }
+            }
+
+//                        System.out.println();
+
+        }
+        return k;
     }
 
 
--- src/uz/mirzokhidkh/adventofcode/y2023/day16/Util.java
+++ src/uz/mirzokhidkh/adventofcode/y2023/day16/Util.java
@@ -1,7 +1,116 @@
 package uz.mirzokhidkh.adventofcode.y2023.day16;
 
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
 public class Util {
 
+    public static final String RIGHT = "RIGHT";
+    public static final String LEFT = "LEFT";
+    public static final String UP = "UP";
+    public static final String DOWN = "DOWN";
+
+
+    public static int bfs(char[][] grid, Queue<Tile> queue, boolean[][] visited) {
+
+//        String from;
+        int count = 0;
+        Set<String> set = new HashSet<>();
+//        System.out.println(i+'-'+j);
+//        visited[i][j] = true;
+
+
+        while (!queue.isEmpty()) {
+
+
+            int size = queue.size();
+
+            for (int k = 0; k < size; k++) {
+                Tile tile = queue.poll();
+                int i = tile.i;
+                int j = tile.j;
+                String from = tile.from;
+
+
+                if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+                    continue;
+                }
+
+//                System.out.println(i + "-" + j);
+
+//                boolean isRepeat = false;
+//                for (String s : set) {
+//                    if (s.startsWith(i + "-" + j)) {
+//                        isRepeat = true;
+//                        break;
+//                    }
+//                }
+
+//                if (!isRepeat) {
+//                    ++count;
+//                }
+
+//                if (!visited[i][i]) {
+//                    ++count;
+//                }
+
+
+                String key = i + "-" + j + "-" + from;
+                if (set.contains(key)) {
+                    continue;
+                }
+                set.add(key);
+
+                char c = grid[i][j];
+                visited[i][j] = true;
+
+
+                if (c == '/') {
+                    switch (from) {
+                        case RIGHT -> queue.offer(new Tile(i + 1, j, UP));
+                        case LEFT -> queue.offer(new Tile(i - 1, j, DOWN));
+                        case UP -> queue.offer(new Tile(i, j - 1, RIGHT));
+                        case DOWN -> queue.offer(new Tile(i, j + 1, LEFT));
+                    }
+                } else if (c == '\\') {
+                    switch (from) {
+                        case RIGHT -> queue.offer(new Tile(i - 1, j, DOWN));
+                        case LEFT -> queue.offer(new Tile(i + 1, j, UP));
+                        case UP -> queue.offer(new Tile(i, j + 1, LEFT));
+                        case DOWN -> queue.offer(new Tile(i, j - 1, RIGHT));
+                    }
+                } else if (c == '|') {
+                    switch (from) {
+                        case RIGHT, LEFT -> {
+                            queue.offer(new Tile(i - 1, j, DOWN));
+                            queue.offer(new Tile(i + 1, j, UP));
+                        }
+                        case UP -> queue.offer(new Tile(i + 1, j, UP));
+                        case DOWN -> queue.offer(new Tile(i - 1, j, DOWN));
+                    }
+                } else if (c == '-') {
+                    switch (from) {
+                        case UP, DOWN -> {
+                            queue.offer(new Tile(i, j - 1, RIGHT));
+                            queue.offer(new Tile(i, j + 1, LEFT));
+                        }
+                        case RIGHT -> queue.offer(new Tile(i, j - 1, RIGHT));
+                        case LEFT -> queue.offer(new Tile(i, j + 1, LEFT));
+                    }
+                } else {
+                    switch (from) {
+                        case RIGHT -> queue.offer(new Tile(i, j - 1, RIGHT));
+                        case LEFT -> queue.offer(new Tile(i, j + 1, LEFT));
+                        case UP -> queue.offer(new Tile(i + 1, j, UP));
+                        case DOWN -> queue.offer(new Tile(i - 1, j, DOWN));
+                    }
+                }
+            }
+
+        }
+        return count;
+    }
 
 
 }
