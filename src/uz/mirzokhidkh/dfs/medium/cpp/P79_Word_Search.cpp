d14fc43394a341f907dbd3a705f6fa5a2d8cc1a5
mirzokhid.khasanov <mirzokhid.khasanov@fido.uz>
2024-03-29 16:19:01 +0500

    dfs p-solution

--- src/uz/mirzokhidkh/dfs/medium/cpp/P79_Word_Search.cpp
+++ src/uz/mirzokhidkh/dfs/medium/cpp/P79_Word_Search.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
+    // int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     bool exist(vector<vector<char>>& board, string word) {
 
         for(int i = 0; i < board.size(); i++){
             for(int j = 0; j < board[0].size(); j++){
-                // vector<vector<bool>> visited(, vector<bool>(, false ));
-                vector<vector<bool>> visited(board.size(), vector<bool> (board[0].size(), false));
-
-                if(dfs(board,i,j,visited,word,0)){
+                if(dfs(board,i,j,word,0)){
                     return true;
                 }
             }
@@ -16,29 +15,37 @@ public:
         return false;
     }
 
-    bool dfs(vector<vector<char>>& grid, int i, int j,vector<vector<bool>> visited,string word, int pos){
-        if(pos == word.length()) return true;
+    bool dfs(vector<vector<char>>& grid, int i, int j,string word, int pos){
+        if(pos >= word.length()) return true;
 
-        if(!isValidIndex(grid.size(),grid[0].size(),i,j,visited)) return false;
+        // if(!isValid(grid,i,j, word[pos])) return false;
+       if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != word[pos])
+            return false;
 
-        if(grid[i][j] != word[pos]) return false;
+        grid[i][j] = '#';
 
-        visited[i][j] = true;
+        // cout<<"grid["<<i<<"]["<<j<<"] = "<<grid[i][j]<<endl;
 
-        if(grid[i][j] == word[pos]) {
-            cout<<"grid["<<i<<"]["<<j<<"] = "<<grid[i][j]<<endl;
-        }
+        bool up    = dfs(grid,i-1,j,word,pos+1);
+        // if(up) return true;
 
-        bool up = dfs(grid,i-1,j,visited,word,pos+1);
-        bool down = dfs(grid,i+1,j,visited,word,pos+1);
-        bool left = dfs(grid,i,j-1, visited, word,pos+1);
-        bool right = dfs(grid,i,j+1, visited, word,pos+1);
+        bool right = dfs(grid,i,j+1,word,pos+1);
+        // if(right) return true;
 
-        return up || down || left || right;
+        bool down  = dfs(grid,i+1,j,word,pos+1);
+        // if(down) return true;
+
+        bool left  = dfs(grid,i,j-1,word,pos+1);
+
+        grid[i][j] = word[pos];
+
+        // return left;
+
+        return up || right || down || left ;
     }
 
 
-    bool isValidIndex(int m, int n, int i, int j,vector<vector<bool>> visited){
-        return i > -1 && i < m && j > -1 && j < n && !visited[i][j];
-    }
+    // bool isValid(vector<vector<char>> grid, int i, int j, char c){
+    //     return i > -1 && i < grid.size() && j > -1 && j < grid[0].size() && grid[i][j] == c;
+    // }
 };
